40520c0e192544c52914f4a372b69502493612f4
yuma140902 <yuma140902@gmail.com>
2019-06-06 19:30:58 +0900

    IHasRecipeを追加

--- src/main/java/yuma140902/lepton/recipes/Recipes.java
+++ src/main/java/yuma140902/lepton/recipes/Recipes.java
@@ -1,7 +1,10 @@
 package yuma140902.lepton.recipes;
 
+import java.util.Iterator;
+import net.minecraft.block.Block;
 import yuma140902.lepton.handlers.ClayConversionHandler;
 import yuma140902.lepton.handlers.DyeAnyWoolHandler;
+import yuma140902.lepton.registry.MyBlocks;
 
 public class Recipes {
 	private Recipes() {}
@@ -9,5 +12,13 @@ public class Recipes {
 	public static void register() {
 		ClayConversionHandler.registerRecipe();
 		DyeAnyWoolHandler.registerRecipes();
+		
+		Iterator<Block> iter = MyBlocks.iterator();
+		while (iter.hasNext()) {
+			Block block = iter.next();
+			if(block instanceof IHasRecipe) {
+				((IHasRecipe) block).registerRecipe();
+			}
+		}
 	}
 }
